6af2996ab6b93d10043adff36b809565ddacb966
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2022-07-08 00:45:34 +0300

    Data - AppDbContext eklendi. DbSetler yazıldı.

---
+++ BlogApp.Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using BlogApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<AppRole> AppRoles { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AppUserRole> AppUserRoles { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<TagBlog> TagBlogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
