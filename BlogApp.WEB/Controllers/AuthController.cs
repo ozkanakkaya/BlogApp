d4f498e8a78eedc20f91051cc7b4a5090b109c34
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-07-05 19:38:27 +0300

    WEB -  AuthController/Register yapıldı

--- BlogApp.WEB/Controllers/AuthController.cs
+++ BlogApp.WEB/Controllers/AuthController.cs
@@ -1,21 +1,61 @@
 ﻿using BlogApp.Core.DTOs.Concrete;
+using BlogApp.Core.Enums.ComplexTypes;
+using BlogApp.Core.Enums;
+using BlogApp.WEB.Models;
 using BlogApp.WEB.Services;
+using BlogApp.WEB.Utilities.Extensions;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using BlogApp.Core.Utilities.Abstract;
+using System.Text.Json;
 
 namespace BlogApp.WEB.Controllers
 {
     public class AuthController : Controller
     {
         private readonly AuthApiService _authApiService;
+        private readonly UserApiService _userApiService;
+        private readonly IImageHelper _imageHelper;
 
-        public AuthController(AuthApiService authApiService)
+        public AuthController(AuthApiService authApiService, IImageHelper imageHelper, UserApiService userApiService)
         {
             _authApiService = authApiService;
+            _imageHelper = imageHelper;
+            _userApiService = userApiService;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterDto registerDto)
+        {
+            var uploadedImageDtoResult = await _imageHelper.UploadAsync(registerDto.Username, registerDto.ImageFile, ImageType.User);
+            registerDto.ImageUrl = uploadedImageDtoResult.StatusCode == 200 ? uploadedImageDtoResult.Data.FullName : "userImages/defaultUser.png";
+
+            var userDto = await _userApiService.RegisterAsync(registerDto);
+
+            if (!userDto.Errors.Any())
+            {
+                return RedirectToAction("LogIn", "Auth");
+            }
+            else
+            {
+                if (registerDto.ImageUrl != "userImages/defaultUser.png")
+                    await _imageHelper.DeleteAsync(registerDto.ImageUrl);
+
+                foreach (var error in userDto.Errors)
+                {
+                    ModelState.AddModelError("", error);
+                }
+                return View();
+            }
         }
 
         public IActionResult LogIn()
@@ -27,7 +67,7 @@ namespace BlogApp.WEB.Controllers
         public async Task<IActionResult> LogIn(UserLoginDto loginDto)
         {
             var tokenModel = await _authApiService.LogIn(loginDto);
-            
+
             if (!tokenModel.Errors.Any())
             {
                 JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
@@ -49,7 +89,7 @@ namespace BlogApp.WEB.Controllers
 
                     await HttpContext.SignInAsync(JwtBearerDefaults.AuthenticationScheme, new ClaimsPrincipal(identity), authProps);
 
-                    return RedirectToAction("Index", "Home", new { Area = "Admin" });
+                    return RedirectToAction("Index", "Home"/*, new { Area = "Admin" }*/);
                 }
                 else
                 {
